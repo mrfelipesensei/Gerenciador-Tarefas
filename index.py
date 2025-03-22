742a455ff1df6684235dbc531673ce6eeb9ebd28
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-22 10:36:12 -0300

    main() v1.0

--- index.py
+++ index.py
@@ -29,11 +29,40 @@ def salvar_json(tarefas,data,nova_tarefa):
     with open(arquivo_json,"w") as file:
         json.dump(tarefas,file,indent=4) #Salva com identação para melhor legibilidade
 
-#Exemplo de uso
+'''#Exemplo de uso
 tarefas = ler_json()
 data_atual = "2025-03-22"
 nova_tarefa = {"nome":"Reunião","concluida":False}
 #Salva a nova tarefa no arquivo
 salvar_json(tarefas,data_atual,nova_tarefa)
 #Exibe o conteúdo atual das tarefas
-print(tarefas)
+print(tarefas)'''
+
+#Função principal
+def main():
+    tarefas = ler_json()
+
+    #Inserção de dados pelo usuário
+    data_atual = input("Digite a data (YYY-MM-DD): ")
+    nome_tarefa = input("Digite o nome da tarefa: ")
+    concluida_input = input("A tarefa está concluída? ").strip().lower()
+
+    #Conversão "Sim" para True e "Não" para False
+    if concluida_input == "sim":
+        concluida = True
+    elif concluida_input == "não":
+        concluida = False
+    else:
+        print("Entrada inválida para 'concluida'. Usando False por padrão.")
+        concluida = False
+
+    nova_tarefa = {"nome": nome_tarefa, "concluida": concluida} #Criando o dicionário da nova tarefa
+
+    salvar_json(tarefas,data_atual,nova_tarefa)
+
+    print(f"Tarefa '{nova_tarefa}' adicionada para o dia {data_atual}.")
+    print("Tarefas atuais:",tarefas)
+
+
+if __name__ == "__main__":
+    main()
