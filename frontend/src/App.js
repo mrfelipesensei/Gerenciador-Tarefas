8ec7f6b0ed5e88b2e82275210c0c31929e085c67
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-22 22:46:36 -0300

    app.js pt 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const [quantidade, setQuantidade] = useState(0);
+  const [tarefas, setTarefas] = useState([]);
+  const [tarefasSalvas, setTarefasSalvas] = useState({});
+  const [message, setMessage] = useState("");
+
+  //Carrega todas as tarefas do backend
+  const carregarTarefas = () => {
+    fetch("http://127.0.0.1:5000/tarefas")
+    .then((res) => res.json())
+    .then((data) => setTarefasSalvas(data))
+    .catch((error) => console.error("Erro ao carregar tarefas:",console.error));
+  };
+
+  useEffect(() => {
+    carregarTarefas();
+  },[]);
+
+  //Gera os campos para a quantidade de tarefas especificada
+  const gerarCampos = () => {
+    const novosCampos = Array.from({length:Number(quantidade)}, () =>({
+      nome: "",
+      concluida: false,
+    }));
+    setTarefas(novosCampos);
+  };
+
+  //Atualiza os dados de cada tarefa
+  const handleTarefaChange = (index, campo, valor) => {
+    const novasTarefas = [...tarefas];
+    novasTarefas[index][campo] = valor;
+    setTarefas(novasTarefas);
+  };
+
+  //Envia as tarefas para o backend
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!data || tarefas.length === 0){
+      setMessage("Data e tarefas são obrigatórios.");
+      return;
+    }
+    
+    fetch("http://127.0.0.1:5000/tarefas",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({data, tarefas}),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.message){
+          setMessage(result.message);
+        }else if (result.error) {
+          setMessage(result.error);
+        }
+        carregarTarefas();
+        //Limpa os campos do formulário
+        setData("");
+        setQuantidade(0);
+        setTarefas([]);
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+        setMessage("Erro ao salvar tarefas.")
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  )
+
+
+
 }
 
 export default App;
